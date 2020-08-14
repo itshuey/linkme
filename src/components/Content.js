a14bbf4bb3084bf00e3eb1a53e9cc126f9a5ca83
itshuey <hubert.s.sun@gmail.com>
2020-08-13 18:11:26 -0700

    Release version 1

---
+++ src/components/Content.js
@@ -0,0 +1,136 @@
+import React, { Component } from "react";
+import Fade from 'react-reveal/Fade';
+import './Content.css'
+
+export default class Content extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      archive: false,
+      selected: "main",
+      image: "none",
+    };
+  }
+
+  renderContent() {
+
+    const current_info = [
+      {
+        desc: "Here's a song stuck in my head",
+        space: " .........................",
+        mobile_space: ".........................",
+        dest:
+          <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.youtube.com/watch?v=Z417ESyz3oQ"
+          onMouseOver={() => this.setState({image : "MUNA"})}
+          onMouseLeave={() => this.setState({image : "none"})}
+          >
+          Number One Fan
+          </a>,
+      },
+      {
+        desc: "Something I wrote",
+        space: "...............................................",
+        mobile_space: ".........................",
+        dest: <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.notion.so/The-World-Is-Not-Enough-1999-2fb147cc9b0b4bf4b0a7669bf9f399d4"
+          onMouseOver={() => this.setState({image : "BOND"})}
+          onMouseLeave={() => this.setState({image : "none"})}
+          >
+          Movie Review
+          </a>,
+      },
+      {
+        desc: "My current favorite color",
+        space: "........................................",
+        dest: <span class="color"> FF4500 </span>,
+      },
+      {
+        desc: "Some art I made",
+        space: "..............................................",
+        dest: <span
+          class="crosshair"
+          onMouseOver={() => this.setState({image : "ART"})}
+          onMouseLeave={() => this.setState({image : "none"})}>
+        Audrey Vol. 1
+        </span>,
+      }
+    ]
+
+    const archive_info = [
+      {
+        date: "08/12/2020",
+        dest: "launch",
+      },
+      {
+        date: "08/13/2020",
+        dest: "current",
+      }
+    ]
+
+    let current = current_info.map((item) =>
+      <div> {item.desc} {item.space} {item.dest} </div>
+    );
+
+    const archive = archive_info.map((item) =>
+      <div> {item.date} {"::::::::::"} {item.dest} </div>
+    );
+
+    const content = (
+      <div class="links">
+        {this.state.archive ? archive : current}
+      </div>
+    )
+
+    const header = (
+      <div class="header">
+        <h1><Fade bottom cascade>{this.state.archive ? <i>archive</i> : "What I'm Up To:"}</Fade></h1>
+      </div>
+    );
+
+    return (
+      <div class={this.state.image === "none" ? "content" : "content clear"}>
+        {header}
+        {content}
+      </div>
+    )
+  }
+
+  render() {
+
+    const page = (
+      <div class="main">
+        {this.renderContent()}
+        <div class="toggle" onClick={()=> this.setState({archive: !this.state.archive})}>
+          <i>{this.state.archive ? "home" : "archive"}</i>
+        </div>
+        <div class="footer concept"> linkme </div>
+        <div class="footer signature"> by huey </div>
+        {this.state.image === "BOND" &&
+          <img class="hovered"
+          onClick={() => this.setState({archive: true})}
+          width="80%"
+          src="https://jamesbondradio.com/wp-content/uploads/2018/12/the-world-is-not-enough.png">
+          </img>
+        }
+        {this.state.image === "MUNA" &&
+            <img class="hovered"
+            src="https://static.gigwise.com/gallery/107182/muna-justinetrickett-3105.jpg">
+            </img>
+        }
+        {this.state.image === "ART" &&
+            <img class="hovered"
+            width="70%"
+            src="https://i.imgur.com/uudajbN.jpg">
+            </img>
+        }
+      </div>
+    );
+
+    return page;
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,6 @@
-import React from "react"
-import './Page.css'
+import React, { Component } from "react";
+import Content from "../components/Content.js"
 
 export default function Home() {
-  const page = (<body>
-      <div class="header">
-        <h1>What I'm Up To:</h1>
-      </div>
-      <div class="links">
-      Here's a song stuck in my head ......................... Please (Rhye) <br/>
-      Something I wrote: .............................................. Movie Review  <br/>
-      My current favorite color ........................................ <span class="color"> FF4500 </span><br/>
-      A meme you might like .............................................. This <br/>
-      </div>
-      <div class="interact">
-
-      </div>
-      <div class="concept">
-       linkme
-      </div>
-      <div class="archive">
-       <i>archive</i>
-      </div>
-      <div class="signature">
-        by huey
-      </div>
-    </body>);
-
-  return page;
+  return <Content ></Content>
 }
